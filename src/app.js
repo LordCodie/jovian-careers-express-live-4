2ed9e63523c803aedf690068518bde8661babbfb
LordCodie <153275673+LordCodie@users.noreply.github.com>
2024-03-07 09:54:02 +0000

    last did: Route Parameters & Dynamic Pages

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
     // res.send('Hello World!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS});
 });
+
+app.get('/jobs/:id', (req, res) => {
+    console.log('req.params', req.params);
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    console.log('matchedJob', matchedJob);
+    res.render('job', { job: matchedJob});
+})
 
 const port = process.env.PORT || 3000;
 
